e4745bdf322303b48e726bf17d288a5932bbfae5
Savan Savani <savansavani111@gmail.com>
2023-02-18 22:21:12 -0500

    firebase config

--- src/App.js
+++ src/App.js
@@ -4,26 +4,54 @@ import Header from "./components/header/Header";
 import Sidebar from "./components/sidebar/Sidebar";
 import HomeScreen from "./screens/homeScreen/HomeScreen";
 import LoginScreen from "./screens/LoginScreen.js/LoginScreen";
+
+import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
+
 import "./_app.scss";
 
-const App = () => {
+const Layout = ({ children }) => {
   const [sidebar, toggleSidebar] = useState(false);
 
   const handleToggleSidebar = () => {
     toggleSidebar((value) => !value);
   };
-
   return (
-    // <div>
-    //   <Header handleToggleSidebar={handleToggleSidebar}/>
-    //   <div className="app__container ">
-    //     <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar}/>
-    //     <Container className="app__main " fluid>
-    //       <HomeScreen />
-    //     </Container>
-    //   </div>
-    // </div>
-    <LoginScreen />
+    <>
+      <Header handleToggleSidebar={handleToggleSidebar} />
+      <div className="app__container ">
+        <Sidebar sidebar={sidebar} handleToggleSidebar={handleToggleSidebar} />
+        <Container className="app__main " fluid>
+          {children}
+        </Container>
+      </div>
+    </>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <HomeScreen />
+            </Layout>
+          }
+        />
+        <Route path="/auth" element={<LoginScreen />} />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <h1>search results</h1>
+            </Layout>
+          }
+        />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   );
 };
 
---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCg-DLaXfCtgxXZJju11XTOEbbhHl8fIhI",
+    authDomain: "yt-clone-by-savan.firebaseapp.com",
+    projectId: "yt-clone-by-savan",
+    storageBucket: "yt-clone-by-savan.appspot.com",
+    messagingSenderId: "355035225483",
+    appId: "1:355035225483:web:3022a7a770533c6f12a38e"
+  };
+
+  firebase.initializeApp(firebaseConfig)
