0d48dc720335c967d21605c322208c617ff1b0b7
Savan Savani <savansavani111@gmail.com>
2023-03-31 18:39:49 -0400

    google html file add

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from "react-redux";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { Helmet } from "react-helmet";
 
 import "./_base.scss";
 import store from "./redux/store";
@@ -15,12 +14,6 @@ const root = createRoot(container);
 
 root.render(
   <div>
-    <Helmet>
-      <meta
-        name="google-site-verification"
-        content="YKCJ7R1TzohJNu5er1VY0HqotmCCXiRXvzfxDdTsBlQ"
-      />
-    </Helmet>
     <Provider store={store}>
       <Router>
         <App />
